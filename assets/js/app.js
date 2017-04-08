ff34c1c7a9a7b3699eb6c4306aa3466a4e9ddf5e
Yelitza Cho <yelitza.choque@gmail.com>
2017-04-07 23:43:14 -0500

    Subiendo cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,31 @@
+document.getElementById("toggle").addEventListener("click", function(evento){
+  evento.preventDefault();
+  document.getElementById("nav-header").classList.toggle("open");
+  document.getElementById('body').classList.toggle("overflow-hidden");
+});
+
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function(){
+   var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+   if (currentScroll > lastScrollTop){
+     document.getElementById('nav-background').style.display="none";
+     document.getElementById("nav-header").classList.remove("header-in");
+     document.getElementById("nav-header").style.opacity = 0;
+   } else {
+     document.getElementById('nav-background').style.display="table";
+     document.getElementById("nav-header").classList.add("header-in");
+     document.getElementById("nav-header").classList.add("solid");
+     document.getElementById("nav-header").style.opacity = 1;
+     if(currentScroll<=3){
+       document.getElementById("nav-header").classList.remove("solid");
+       document.getElementById("nav-header").classList.remove("header-in");
+     }
+   }
+
+   lastScrollTop = currentScroll;
+}, false);
+
 window.addEventListener('load', function(evento){
   event.preventDefault();
 
